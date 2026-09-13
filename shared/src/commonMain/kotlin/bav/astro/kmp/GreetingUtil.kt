ce8530bd7e67a5ebf0614a6e09ad5e8a740b25aa
Aleksandr Bakanov <bakanov.aleksandr@gmail.com>
2026-09-13 12:55:27 +0200

    Remove unused code

--- shared/src/commonMain/kotlin/bav/astro/kmp/GreetingUtil.kt
+++
@@ -1,4 +0,0 @@
-package bav.astro.kmp
-
-fun sayHello(to: String): String =
-    "Hello, $to!"
--- shared/src/commonMain/kotlin/bav/astro/kmp/Platform.kt
+++
@@ -1,7 +0,0 @@
-package bav.astro.kmp
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
